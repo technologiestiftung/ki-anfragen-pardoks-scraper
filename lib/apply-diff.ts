dc2320b44cdb5a431d28b1590786b0f7baee78ca
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2023-08-30 14:45:05 +0200

    feat: diff script (#5)
    * feat: diff script
    
    * chore: remove umlaute, insert vorgang refactoring, parameter naming
    
    * feat: ignore deleted vorgangs both in diff and write script
    
    * chore: exit with errors on thrown exceptions

---
+++ lib/apply-diff.ts
@@ -0,0 +1,96 @@
+/* eslint-disable indent */
+import progress from "progress";
+import { ParDoks } from "./common.js";
+import { connectDB } from "./sql.js";
+import { insertVorgangs } from "./write-to-db.js";
+
+export async function applyDiff(newParDoks: ParDoks, dbUrl: string) {
+	const sql = connectDB(dbUrl);
+
+	try {
+		const dbExport = await sql`select * from export;`;
+		if (!dbExport || dbExport.length === 0) {
+			throw new Error("No export found in database.");
+		}
+
+		// Check if update times are okay
+		const newPardoksUpdatedAt = new Date(
+			newParDoks.Export.$.aktualisiert,
+		).getTime();
+		const remoteUpdatedAt = new Date(dbExport[0].aktualisiert).getTime();
+
+		if (newPardoksUpdatedAt < remoteUpdatedAt) {
+			throw new Error(
+				"Export update time in database is more recent than local update time. Refusing to update.",
+			);
+		}
+
+		const updatedDiffDays =
+			(newPardoksUpdatedAt - remoteUpdatedAt) / 1000 / 60 / 60 / 24;
+		console.log(
+			`Remote database is behind local file by ${Math.round(
+				updatedDiffDays,
+			)} days...`,
+		);
+
+		console.log("Loading local and remote Vorgangs...");
+		// Get all undeleted local Vorgangs
+		const undeletedLocalVorgangs = newParDoks.Export.Vorgang.filter((v) => {
+			return (
+				!v.VFunktion || v.VFunktion.length === 0 || v.VFunktion[0] !== "delete"
+			);
+		});
+
+		// Get all (undeleted) remote Vorgangs
+		const undeletedRemoteVorgangs = await sql`select * from vorgang`;
+
+		// Get Vorgangs which are present on remote, but not present locally -> those should be removed
+		console.log("Finding Vorgangs which should be deleted...");
+		const remoteVorgangsToDelete = undeletedRemoteVorgangs.filter((rv) => {
+			return (
+				undeletedLocalVorgangs.filter((v) => v.VNr[0] === rv.vnr).length === 0
+			);
+		});
+
+		// Get Vorgangs which are present locally, but not remote -> those should be added
+		console.log("Finding Vorgangs which should be added...");
+		const localVorgangsToAddToRemote = undeletedLocalVorgangs.filter((lv) => {
+			return (
+				undeletedRemoteVorgangs.filter((v) => v.vnr === lv.VNr[0]).length === 0
+			);
+		});
+
+		// Deleting Vorgangs in database
+		if (remoteVorgangsToDelete.length === 0) {
+			console.log("No Vorgangs to delete in database...");
+		} else {
+			console.log(`Deleting Vorgangs in database...`);
+			const progressBarDelete = new progress(":bar :current/:total", {
+				total: remoteVorgangsToDelete.length,
+			});
+			for (const vorgang of remoteVorgangsToDelete) {
+				await sql`delete from vorgang where vnr = ${vorgang.vnr}`;
+				progressBarDelete.tick();
+			}
+			progressBarDelete.terminate();
+		}
+
+		// Adding Vorgangs to database
+		if (localVorgangsToAddToRemote.length === 0) {
+			console.log("No Vorgangs to add to database...");
+		} else {
+			console.log(`Adding local Vorgangs to database...`);
+			await insertVorgangs(localVorgangsToAddToRemote, dbExport[0].id, dbUrl);
+		}
+
+		// Set new update time to export
+		await sql`update export set aktualisiert = ${newParDoks.Export.$.aktualisiert} where id = ${dbExport[0].id}`;
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	} finally {
+		sql.end();
+	}
+
+	return;
+}
--- lib/write-to-db.ts
+++ lib/write-to-db.ts
@@ -4,6 +4,75 @@ import progress from "progress";
 import { checkIfArray } from "./utils.js";
 import { connectDB } from "./sql.js";
 
+export async function insertVorgangs(
+	vorgangs: Array<Vorgang>,
+	exportId: number,
+	url: string,
+) {
+	const sql = connectDB(url);
+	// Create a new progress bar with the length of the Vorgang array
+	const progressBarInsert = new progress(":bar :current/:total", {
+		total: vorgangs.length,
+	});
+	for (const vorgang of vorgangs) {
+		const vorgangResult = await sql`
+		INSERT INTO vorgang (VNr, VFunktion, ReihNr, VTyp, VTypL, VSys, VSysL, VIR, export_id)
+		VALUES (${checkIfArray(vorgang.VNr)}, ${checkIfArray(
+			vorgang.VFunktion,
+		)}, ${checkIfArray(vorgang.ReihNr)}, ${checkIfArray(
+			vorgang.VTyp,
+		)}, ${checkIfArray(vorgang.VTypL)}, ${checkIfArray(
+			vorgang.VSys,
+		)}, ${checkIfArray(vorgang.VSysL)}, ${checkIfArray(
+			vorgang.VIR,
+		)}, ${exportId})
+	RETURNING id
+  `;
+
+		if (vorgang.Nebeneintrag) {
+			for (const nebeneintrag of vorgang.Nebeneintrag) {
+				await sql`
+			INSERT INTO nebeneintrag (ReihNr, Desk, vorgang_id)
+			VALUES (${checkIfArray(nebeneintrag.ReihNr)}, ${checkIfArray(
+				nebeneintrag.Desk,
+			)}, ${vorgangResult[0].id})
+		`;
+			}
+		}
+		if (vorgang.Dokument) {
+			for (const dokument of vorgang.Dokument) {
+				await sql`
+			INSERT INTO dokument (ReihNr, DHerk, DHerkL, Wp, DokArt, DokArtL, DokTyp, DokTypL, NrInTyp, Desk, Titel, DokNr, DokDat, LokURL, Sb, VkDat, HNr, Jg, Abstract, Urheber, vorgang_id)
+			VALUES (${checkIfArray(dokument.ReihNr)}, ${checkIfArray(
+				dokument.DHerk,
+			)}, ${checkIfArray(dokument.DHerkL)}, ${checkIfArray(
+				dokument.Wp,
+			)}, ${checkIfArray(dokument.DokArt)}, ${checkIfArray(
+				dokument.DokArtL,
+			)}, ${checkIfArray(dokument.DokTyp)}, ${checkIfArray(
+				dokument.DokTypL,
+			)}, ${checkIfArray(dokument.NrInTyp)}, ${checkIfArray(
+				dokument.Desk,
+			)}, ${checkIfArray(dokument.Titel)}, ${checkIfArray(
+				dokument.DokNr,
+			)}, ${checkIfArray(dokument.DokDat)}, ${checkIfArray(
+				dokument.LokURL,
+			)}, ${checkIfArray(dokument.Sb)}, ${checkIfArray(
+				dokument.VkDat,
+			)}, ${checkIfArray(dokument.HNr)}, ${checkIfArray(
+				dokument.Jg,
+			)}, ${checkIfArray(dokument.Abstract)}, ${checkIfArray(
+				dokument.Urheber,
+			)}, ${vorgangResult[0].id})
+		`;
+			}
+		}
+
+		progressBarInsert.tick();
+	}
+	progressBarInsert.terminate();
+}
+
 export async function write2DB({
 	parDoks,
 	url,
@@ -17,88 +86,13 @@ export async function write2DB({
 	const sql = connectDB(url);
 	try {
 		// Call the insertParDoks function with your ParDoks object
-		const exportResult = await sql`
-INSERT INTO export (aktualisiert, filename) VALUES (${parDoks.Export.$.aktualisiert},${filename}) RETURNING id
-    `;
+		const exportResult =
+			await sql`INSERT INTO export (aktualisiert, filename) VALUES (${parDoks.Export.$.aktualisiert},${filename}) RETURNING id`;
 
-		// Create a new progress bar with the length of the Vorgang array
-		const progressBarInsert = new progress(":bar :current/:total", {
-			total: parDoks.Export.Vorgang.length,
-		});
-		for (const vorgang of parDoks.Export.Vorgang) {
-			const vorgangResult = await sql`
-				INSERT INTO vorgang (VNr, VFunktion, ReihNr, VTyp, VTypL, VSys, VSysL, VIR, export_id)
-				VALUES (${checkIfArray(vorgang.VNr)}, ${checkIfArray(
-					vorgang.VFunktion,
-				)}, ${checkIfArray(vorgang.ReihNr)}, ${checkIfArray(
-					vorgang.VTyp,
-				)}, ${checkIfArray(vorgang.VTypL)}, ${checkIfArray(
-					vorgang.VSys,
-				)}, ${checkIfArray(vorgang.VSysL)}, ${checkIfArray(vorgang.VIR)}, ${
-					exportResult[0].id
-				})
-		    RETURNING id
-		  `;
-
-			if (vorgang.Nebeneintrag) {
-				for (const nebeneintrag of vorgang.Nebeneintrag) {
-					await sql`
-					INSERT INTO nebeneintrag (ReihNr, Desk, vorgang_id)
-					VALUES (${checkIfArray(nebeneintrag.ReihNr)}, ${checkIfArray(
-						nebeneintrag.Desk,
-					)}, ${vorgangResult[0].id})
-			    `;
-				}
-			}
-			if (vorgang.Dokument) {
-				for (const dokument of vorgang.Dokument) {
-					await sql`
-					INSERT INTO dokument (ReihNr, DHerk, DHerkL, Wp, DokArt, DokArtL, DokTyp, DokTypL, NrInTyp, Desk, Titel, DokNr, DokDat, LokURL, Sb, VkDat, HNr, Jg, Abstract, Urheber, vorgang_id)
-					VALUES (${checkIfArray(dokument.ReihNr)}, ${checkIfArray(
-						dokument.DHerk,
-					)}, ${checkIfArray(dokument.DHerkL)}, ${checkIfArray(
-						dokument.Wp,
-					)}, ${checkIfArray(dokument.DokArt)}, ${checkIfArray(
-						dokument.DokArtL,
-					)}, ${checkIfArray(dokument.DokTyp)}, ${checkIfArray(
-						dokument.DokTypL,
-					)}, ${checkIfArray(dokument.NrInTyp)}, ${checkIfArray(
-						dokument.Desk,
-					)}, ${checkIfArray(dokument.Titel)}, ${checkIfArray(
-						dokument.DokNr,
-					)}, ${checkIfArray(dokument.DokDat)}, ${checkIfArray(
-						dokument.LokURL,
-					)}, ${checkIfArray(dokument.Sb)}, ${checkIfArray(
-						dokument.VkDat,
-					)}, ${checkIfArray(dokument.HNr)}, ${checkIfArray(
-						dokument.Jg,
-					)}, ${checkIfArray(dokument.Abstract)}, ${checkIfArray(
-						dokument.Urheber,
-					)}, ${vorgangResult[0].id})
-				`;
-				}
-			}
-
-			progressBarInsert.tick();
-		}
-		progressBarInsert.terminate();
-		interface Row extends Vorgang {
-			id: number;
-		}
-		console.log("cleaning up database might take some time...");
-		const rowsToDelete = await sql<Row[]>`SELECT v1.*
-			FROM public.vorgang v1
-			JOIN public.vorgang v2 ON v1.vnr = v2.vnr
-			WHERE v1.vfunktion = 'delete'
-			AND v2.vfunktion IS NULL;`;
-		// Create a new progress bar with the length of the Vorgang array
-		const progressBarDelete = new progress(":bar :current/:total", {
-			total: rowsToDelete.length,
-		});
-		for (const row of rowsToDelete) {
-			await sql`DELETE FROM public.vorgang WHERE id = ${row.id}`;
-			progressBarDelete.tick();
-		}
+		const undeletedVorgangs = parDoks.Export.Vorgang.filter(
+			(vorgang) => !vorgang.VFunktion || vorgang.VFunktion[0] !== "delete",
+		);
+		await insertVorgangs(undeletedVorgangs, exportResult[0].id, url);
 	} catch (error) {
 		console.error(error);
 	} finally {
